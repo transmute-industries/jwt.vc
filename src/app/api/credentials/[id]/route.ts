9b7acd1c9f62093946de06733bd7b421bcfaca20
Orie Steele <orie@transmute.industries>
2024-04-24 18:43:39 -0500

    fix quoted token

--- src/app/api/credentials/[id]/route.ts
+++ src/app/api/credentials/[id]/route.ts
@@ -6,7 +6,12 @@ import { passport } from '@/services/passport';
 export const GET = async (req: any, {params}) => {
   try {
     const credential = await passport.create({ id : params.id})
-    return Response.json(credential, { status: 200 });
+    return new Response(credential, {
+      status: 200,
+      headers: {
+        // "Content-Type": "application/vc+ld+json+jwt",
+      },
+    });
   } catch (e){
     return Response.json({ message: 'Error' }, { status: 500 });
   }
--- src/app/api/credentials/status/[id]/route.ts
+++ src/app/api/credentials/status/[id]/route.ts
@@ -1,18 +1,30 @@
+import { passport } from "@/services/passport";
 
-import { passport } from '@/services/passport';
-
-export const GET = async (req: any, {params}) => {
+export const GET = async (req: any, { params }) => {
   try {
-    const credential = await passport.status({ id : params.id})
-    return Response.json(credential, { status: 200, headers: { 
-      // "Content-Type": "application/vc+ld+json+jwt" 
-    }  });
-  } catch (e){
-    return Response.json({
-      "type": "https://www.w3.org/ns/credentials/status-list#",
-      "title": "List Unavailable",
-      "detail": "The resource you requested does not exist, or you do not have access to it.",
-      "code": -128
-     }, { status: 404, headers: { "Content-Type": "application/problem+json", "Content-Language": "en" } });
+    const credential = await passport.status({ id: params.id });
+    return new Response(credential, {
+      status: 200,
+      headers: {
+        // "Content-Type": "application/vc+ld+json+jwt",
+      },
+    });
+  } catch (e) {
+    return Response.json(
+      {
+        type: "https://www.w3.org/ns/credentials/status-list#",
+        title: "List Unavailable",
+        detail:
+          "The resource you requested does not exist, or you do not have access to it.",
+        code: -128,
+      },
+      {
+        status: 404,
+        headers: {
+          "Content-Type": "application/problem+json",
+          "Content-Language": "en",
+        },
+      }
+    );
   }
-}
+};
