4ad90d6ca386854d5b7f812f23c50f4adf3a8496
Orie Steele <orie@transmute.industries>
2024-04-25 16:55:20 -0500

    update

--- src/components/PassportVerified/PassportCard.tsx
+++ src/components/PassportVerified/PassportCard.tsx
@@ -47,7 +47,7 @@ function a11yProps(index: number) {
 }
 
 export default function PassportCard({ validation, json }: any) {
-  const [value, setValue] = React.useState(1);
+  const [value, setValue] = React.useState(0);
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
